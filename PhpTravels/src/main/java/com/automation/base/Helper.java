f43d0c3aabebffd2f7c96e9fecff668cd7f74fc1
gganapathi <gganapathi@10.1.32.100>
2018-09-24 23:30:36 -0400

    added new comment

--- PhpTravels/src/main/java/com/automation/base/Helper.java
+++ PhpTravels/src/main/java/com/automation/base/Helper.java
@@ -1,25 +1,25 @@
 package com.automation.base;
 
 public class Helper {
-	
-	public static void testmethod(){
-	
-		//pass
-		
-	}
-	
-	public static void testmethod2() {
-		
-	}
-	
-	
 
-public static void testmethod3() {
-		
+	public static void testmethod() {
+
+		// pass
+
+	}
+
+	public static void testmethod2() {
+
+	}
+
+	public static void testmethod3() {
+
+	}
+
+	public static void testmethod4() {
+
 	}
 	
-public static void testmethod4() {
-		
-	}
+	//adding comment
 
 }
