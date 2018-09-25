9f380792433fc692043f1bb78f9a78530e21a8a7
deepikagirish <deepika.msec@gmail.com>
2018-09-24 20:41:41 -0400

    Changes to helper class

---
+++ Test/PhpTravels/src/main/java/com/automation/base/Helper.java
@@ -0,0 +1,19 @@
+package com.automation.base;
+
+public class Helper {
+	
+	
+	public static void helperMethod() {
+		
+		///something 
+		
+	}
+	
+	
+public static void helperMethod2() {
+		
+		///something 
+		
+	}
+
+}
