2d0624c6170db28cf6d325147f68ef44c03352fd
deepikagirish <deepika.msec@gmail.com>
2018-09-24 23:29:58 -0400

    added comment

--- PhpTravels/src/main/java/com/automation/base/Helper.java
+++ PhpTravels/src/main/java/com/automation/base/Helper.java
@@ -1,25 +1,24 @@
 package com.automation.base;
 
 public class Helper {
-	
-	public static void testmethod(){
-	
-		//pass
-		
-	}
-	
-	public static void testmethod2() {
-		
-	}
-	
-	
 
-public static void testmethod3() {
-		
+	public static void testmethod() {
+
+		// pass
+
+	}
+
+	public static void testmethod2() {
+
 	}
 	
-public static void testmethod4() {
-		
+	//comment
+	public static void testmethod3() {
+
+	}
+
+	public static void testmethod4() {
+
 	}
 
 }
